1c2fe1f00097d482b3406784b051e54c44f0a0af
planifyit.com <contact@planifyit.com>
2023-09-08 22:56:54 +0200

    Update SunburstWidget.js

--- SunburstWidget.js
+++ SunburstWidget.js
@@ -187,8 +187,8 @@ const partition = data => {
         .startAngle(d => d.x0)
         .endAngle(d => d.x1)
         .innerRadius(d => d.y0 * radius * 0.9)
-       .outerRadius(d => d.y0 * radius + (d.y1 - d.y0) * radius * 1.1);
-
+      .outerRadius(d => d.y1 * radius);
+      
 const svg = d3.select(this._shadowRoot.getElementById('chart')).append("svg")
     .attr("viewBox", `0 0 ${width} ${height}`)
     .attr("width", "100%")
